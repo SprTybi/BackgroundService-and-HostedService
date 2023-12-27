1df0af797acae300b6e8068f2031e21b7bfdabc2
Sprtybi <Sprtybi@gmail.com>
2023-12-27 15:36:21 +0330

    Hosted Service and BackgroundService Host Service => Short Task or costomize for long task BackgroundService => for long running Task andautomatic customized

--- BackgroundServices/Program.cs
+++ BackgroundServices/Program.cs
@@ -3,6 +3,7 @@ using BackgroundServices;
 IHost host = Host.CreateDefaultBuilder(args)
     .ConfigureServices(services =>
     {
+        services.AddHostedService<Worker>();
         services.AddHostedService<HostedWorker>();
     })
     .Build();
